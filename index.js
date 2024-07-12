f8b2c5f60deefec2a26a6bd6c95045977ee902c8
Vaibhav Naik <vaibhavnaik2004@gmail.com>
2024-07-12 17:51:50 +0530

    Add `Express`

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello World'));
+app.listen(3000, () => console.log('Server is ready'));
+
+
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
